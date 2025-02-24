b591b86cb5a8e55882b105268093da8d2998d1f1
Suresh-18-11 <161235485+Suresh-18-11@users.noreply.github.com>
2025-02-24 08:27:13 -0500

    Update cad_risk_app.py

--- cad_risk_app.py
+++ cad_risk_app.py
@@ -2,7 +2,12 @@ import streamlit as st
 import numpy as np
 try:
     import matplotlib.pyplot as plt
-    try:
+    import seaborn as sns
+    SEABORN_AVAILABLE = True
+    MATPLOTLIB_AVAILABLE = True
+except ImportError:
+    SEABORN_AVAILABLE = False
+    MATPLOTLIB_AVAILABLE = False
     import seaborn as sns
     SEABORN_AVAILABLE = True
 except ImportError:
